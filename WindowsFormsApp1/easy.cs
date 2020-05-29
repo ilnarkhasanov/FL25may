ddc330fa0f5a2690656e6eca1c545cdec48296b8
Ilnar <4sitescarp@gmail.com>
2020-05-29 19:48:20 +0300

    bug fix

--- WindowsFormsApp1/easy.cs
+++ WindowsFormsApp1/easy.cs
@@ -814,6 +814,8 @@ namespace WindowsFormsApp1
                 res = res + "неправильно\n";
             }
 
+            res = res + "Время: " + minutes.ToString() + ":" + seconds_format(seconds);
+
             MessageBox.Show(res);
         }
 
